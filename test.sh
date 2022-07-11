79da096b4158b7ba1d94ef201886e9ad94c9d230
rollrat <rollrat@naver.com>
2022-07-11 17:44:54 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,9 +2,9 @@ targets=("RDMA-07" "RDMA-09")
 for i in ${targets[@]}
 do
 	ssh ${i} "
-    rm a.c
-    rm a
-    rm result.txt
+		rm a.c
+		rm a
+		rm result.txt
 		wget https://github.com/rollrat/ssh-test-suit/raw/main/a.c
 		gcc a.c -o a
 		./a >> result.txt
