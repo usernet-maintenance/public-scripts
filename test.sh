6b57f8b1ddad6e7ba5303748d8191c5aaca02dd1
rollrat <rollrat@naver.com>
2022-07-11 17:44:41 +0900

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,13 @@
+targets=("RDMA-07" "RDMA-09")
+for i in ${targets[@]}
+do
+	ssh ${i} "
+    rm a.c
+    rm a
+    rm result.txt
+		wget https://github.com/rollrat/ssh-test-suit/raw/main/a.c
+		gcc a.c -o a
+		./a >> result.txt
+	"
+	scp ${i}:result.txt result/result-${i}.txt
+done
