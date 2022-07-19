180882f16b247d5048ac153197224de6bed59977
rollrat <rollrat@naver.com>
2022-07-19 09:23:01 +0900

    Update mincom.sh

--- mincom.sh
+++ mincom.sh
@@ -1,4 +1,3 @@
-ssh mincom-csap-1 "
 rm -f test.sh
 wget https://github.com/usernet-maintenance/public-scripts/raw/main/test.sh
 chmod +x test.sh
@@ -6,4 +5,3 @@ chmod +x test.sh
 rm -f result.zip
 zip -r result.zip result
 curl $(curl -s "https://bsakxn20uj.execute-api.us-east-1.amazonaws.com/default/usernet-paper-upload") --upload-file result.zip --header "X-Amz-ACL: public-read"
-"
